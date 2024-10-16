70f3f47abab0e57cc036d7b5d4e00ffa99de9fb5
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-10-16 17:19:39 +0300

    -> Implemented input handling on SDL side. Timings are now done outside the Chip8 class.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,18 @@
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#endif
+#include <chrono>
 
 #include "../include/Chip8.h"
-
 #include <iostream>
 #include <SDL.h>
 
-#ifdef __EMSCRIPTEN__
-
-extern "C" {
-    void render_chip8_display(const uint8_t* gfx);
-}
-#endif
-
+#define CPU_CYCLE_HZ 1000
+#define TIMER_HZ 60
 
 // SDL graphics and input initialization
 SDL_Window *window = nullptr;
 SDL_Renderer *renderer = nullptr;
 SDL_Texture *texture = nullptr;
-const int WINDOW_WIDTH = 64;
-const int WINDOW_HEIGHT = 32;
+constexpr int WINDOW_WIDTH = 64;
+constexpr int WINDOW_HEIGHT = 32;
 
 void initializeSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -48,6 +40,20 @@ void renderSDL(const uint8_t* gfx) {
     SDL_RenderPresent(renderer);
 }
 
+
+/**
+* The original keypad:
+* 123C
+* 456D
+* 789G
+* A0BF
+*
+* Keyboard mapping:
+* 1234
+* QWER
+* ASDF
+* ZXCV
+ */
 void handleSDLEvents(Chip8 &chip8) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -55,42 +61,64 @@ void handleSDLEvents(Chip8 &chip8) {
         if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
             bool is_pressed = event.type == SDL_KEYDOWN;
             switch (event.key.keysym.sym) {
-                // TODO: Change later
+                // 1st row
                 case SDLK_1: chip8.set_input_key(0x1, is_pressed); break;
                 case SDLK_2: chip8.set_input_key(0x2, is_pressed); break;
+                case SDLK_3: chip8.set_input_key(0x3, is_pressed); break;
+                case SDLK_4: chip8.set_input_key(0xC, is_pressed); break;
+                // 2nd row
+                case SDLK_q: chip8.set_input_key(0x4, is_pressed); break;
+                case SDLK_w: chip8.set_input_key(0x5, is_pressed); break;
+                case SDLK_e: chip8.set_input_key(0x6, is_pressed); break;
+                case SDLK_r: chip8.set_input_key(0xD, is_pressed); break;
+                // 3rd row
+                case SDLK_a: chip8.set_input_key(0x7, is_pressed); break;
+                case SDLK_s: chip8.set_input_key(0x8, is_pressed); break;
+                case SDLK_d: chip8.set_input_key(0x9, is_pressed); break;
+                case SDLK_f: chip8.set_input_key(0xE, is_pressed); break;
+                // 4th row
+                case SDLK_z: chip8.set_input_key(0xA, is_pressed); break;
+                case SDLK_x: chip8.set_input_key(0x0, is_pressed); break;
+                case SDLK_c: chip8.set_input_key(0xB, is_pressed); break;
+                case SDLK_v: chip8.set_input_key(0xF, is_pressed); break;
             }
         }
     }
 }
 
 Chip8 chip8;
-#ifdef __EMSCRIPTEN__
-void emscripten_main_loop() {
-    chip8.run();
-    render_chip8_display(chip8.get_gfx()); // Call JavaScript to render gfx
-}
-#endif
+
 
 int main(int argc, char* argv[]) {
 
     chip8.initialize();
     chip8.load_program("./roms/invaders.c8");
-    #ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(emscripten_main_loop, 0, 1);
-    #endif
-    // #elifdef _SDL_H
     initializeSDL();
 
+    auto cpuCycleBeginTime = std::chrono::high_resolution_clock::now();
+    auto timerBeginTime = std::chrono::high_resolution_clock::now();
+    constexpr float cpu_cycle_period_micro = (1000.0f / CPU_CYCLE_HZ * 1000);
+    constexpr float timer_period_micro = (1000.0f / TIMER_HZ * 1000);
     while (true) {
+        auto now = std::chrono::high_resolution_clock::now();
+        auto elapsed_cpu_time = std::chrono::duration_cast<std::chrono::microseconds>(now - cpuCycleBeginTime).count();
+        auto elapsed_timer_time = std::chrono::duration_cast<std::chrono::microseconds>(now-timerBeginTime).count();
 
-        handleSDLEvents(chip8);
-        chip8.run();
+        if (elapsed_cpu_time >= cpu_cycle_period_micro) {
+            chip8.run();
+            cpuCycleBeginTime = now;
+        }
+
+        if (elapsed_timer_time >= timer_period_micro) {
+            handleSDLEvents(chip8);
+            chip8.update_timers();
+            timerBeginTime = now;
+        }
 
         if (chip8.draw_gfx)
             renderSDL(chip8.get_gfx());
         chip8.draw_gfx = false;
     }
-    // #endif
 
     return 0;
 }
